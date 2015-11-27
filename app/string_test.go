09b9faeeb44daab46c8a2f2c42b38c8e5de3b0cf
Chirantan Mitra <chirantan.mitra@gmail.com>
2015-11-27 22:41:41 +0530

    Use consistent case in test names

--- app/string_test.go
+++ app/string_test.go
@@ -7,19 +7,19 @@ import (
 )
 
 var _ = Describe("String", func() {
-	Context("when searched in slice containing the item", func() {
+	Context("When searched in slice containing the item", func() {
 		It("Exists", func() {
 			Expect(a.StringInSlice("abc", []string{"a", "b", "abc"})).To(BeTrue())
 		})
 	})
 
-	Context("when searched in slice not containing the item", func() {
+	Context("When searched in slice not containing the item", func() {
 		It("Doesn't exist", func() {
 			Expect(a.StringInSlice("abc", []string{"a", "b", "ABC"})).To(BeFalse())
 		})
 	})
 
-	Context("when searched in an empty slice", func() {
+	Context("When searched in an empty slice", func() {
 		It("Doesn't exist", func() {
 			Expect(a.StringInSlice("abc", []string{})).To(BeFalse())
 		})
