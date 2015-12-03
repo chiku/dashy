3ab8921e7c65b2e866b146fcbb45f31aad1e5304
Chirantan Mitra <chirantan.mitra@gmail.com>
2015-12-03 16:25:10 +0530

    Pull out more JS functions

--- public/main.js
+++ public/main.js
@@ -1,39 +1,46 @@
 var virtualDom = require("virtual-dom");
 var nanoajax = require("nanoajax");
 
+var config = window.config;
+
 var h = virtualDom.h;
 var diff = virtualDom.diff;
 var createElement = virtualDom.create;
 var patch = virtualDom.patch;
 
-var config = window.config;
+var buildPipelineTree = function(pipeline) {
+    var stageNodes = pipeline.stages.map(function(stage) {
+        return h("div", {
+            className: "stage " + stage.status.toLowerCase()
+        }, [stage.name]);
+    });
+
+    return h("div", {
+        className: "pipeline"
+    }, [
+        h("div", {
+            className: "stage-container"
+        }, stageNodes),
+        h("div", {
+            className: "pipeline-name"
+        }, [pipeline.name])
+    ]);
+};
 
 var render = function(pipelines) {
-        var pipelinesNodes = pipelines.map(function(pipeline) {
-            var stageNodes = pipeline.stages.map(function(stage) {
-                return h("div", {
-                    className: "stage " + stage.status.toLowerCase()
-                }, [stage.name]);
-            });
+    return h("div", {
+        id: "dashboard"
+    }, pipelines.map(buildPipelineTree));
+};
 
-            return h("div", {
-                className: "pipeline"
-            }, [
-                h("div", {
-                    className: "stage-container"
-                }, stageNodes),
-                h("div", {
-                    className: "pipeline-name"
-                }, [pipeline.name])
-            ]);
-        });
+var renderError = function(message) {
+    return h("div", {
+        id: "dashboard",
+        className: "error"
+    }, message);
+};
 
-        return h("div", {
-            id: "dashboard"
-        }, pipelinesNodes);
-    },
-
-    dash = function() {
+var dash = function() {
         nanoajax.ajax({
             url: "/dashy",
             type: "POST",
@@ -42,14 +49,7 @@ var render = function(pipelines) {
                 interests: config.interests
             })
         }, function(code, responseText, request) {
-            if (code >= 200 && code <= 299) {
-                newTree = render(JSON.parse(responseText));
-            } else {
-                newTree = h("div", {
-                    id: "dashboard",
-                    className: "error"
-                }, [responseText || "Error!"]);
-            }
+            newTree = (code >= 200 && code <= 299) ? render(JSON.parse(responseText)) : renderError(responseText || "Error!");
             patches = diff(tree, newTree);
             rootNode = patch(rootNode, patches);
             tree = newTree;
