ecf5477d40483bba1e04613096752fa29a1a9e1a
Chirantan Mitra <chirantan.mitra@gmail.com>
2015-12-13 17:38:30 +0530

    SimplePipeline slice can be sorted

--- app/simple_dashboard.go
+++ app/simple_dashboard.go
@@ -12,10 +12,16 @@ type SimpleStage struct {
 type SimplePipeline struct {
 	Name   string        `json:"name"`
 	Stages []SimpleStage `json:"stages"`
+	order  int
 }
 type SimpleDashboard struct {
 	Pipelines []SimplePipeline
 	Ignores   []string
+}
+
+func (dashboard SimplePipeline) Order(order int) SimplePipeline {
+	dashboard.order = order
+	return dashboard
 }
 
 func (dashboard *SimpleDashboard) ToJSON() (output []byte, err error) {
@@ -26,3 +32,9 @@ func (dashboard *SimpleDashboard) ToJSON() (output []byte, err error) {
 
 	return output, nil
 }
+
+type ByOrder []SimplePipeline
+
+func (a ByOrder) Len() int           { return len(a) }
+func (a ByOrder) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByOrder) Less(i, j int) bool { return a[i].order < a[j].order }
--- app/simple_dashboard_test.go
+++ app/simple_dashboard_test.go
@@ -1,6 +1,8 @@
 package app_test
 
 import (
+	"sort"
+
 	a "github.com/chiku/dashy/app"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -26,6 +28,26 @@ var _ = Describe("SimpleDashboard", func() {
 			body, err := simpleDashboard.ToJSON()
 			Expect(err).To(BeNil())
 			Expect(string(body)).To(Equal(`[{"name":"Pipeline","stages":[{"name":"Stage","status":"Passed"}]}]`))
+		})
+	})
+
+	Context("Sort by order", func() {
+		It("Sorts in ascending order", func() {
+			p1 := (a.SimplePipeline{
+				Name:   "PipelineOne",
+				Stages: []a.SimpleStage{a.SimpleStage{Name: "Stage", Status: "Passed"}},
+			}).Order(2)
+			p2 := (a.SimplePipeline{
+				Name:   "PipelineTwo",
+				Stages: []a.SimpleStage{a.SimpleStage{Name: "Stage", Status: "Passed"}},
+			}).Order(1)
+			pipelines := []a.SimplePipeline{p1, p2}
+
+			sort.Sort(a.ByOrder(pipelines))
+
+			Expect(pipelines).To(HaveLen(2))
+			Expect(pipelines[0].Name).To(Equal("PipelineTwo"))
+			Expect(pipelines[1].Name).To(Equal("PipelineOne"))
 		})
 	})
 })
