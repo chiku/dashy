b4aa8838e321a16811c87dee83e13e68bd45232c
Chirantan Mitra <chirantan.mitra@gmail.com>
2016-01-27 22:18:48 +0530

    Refactor: extract functions & variables

--- public/main.js
+++ public/main.js
@@ -8,6 +8,12 @@ var diff = virtualDom.diff;
 var createElement = virtualDom.create;
 var patch = virtualDom.patch;
 
+var requestBody = JSON.stringify({
+    url: config.url,
+    interests: config.interests
+});
+var interval = config.interval;
+
 var buildPipelineTree = function(pipeline) {
     var stageNodes = pipeline.stages.map(function(stage) {
         return h("div", {
@@ -27,7 +33,7 @@ var buildPipelineTree = function(pipeline) {
     ]);
 };
 
-var render = function(pipelines) {
+var renderPipeline = function(pipelines) {
     return h("div", {
         id: "dashboard"
     }, pipelines.map(buildPipelineTree));
@@ -40,16 +46,22 @@ var renderError = function(message) {
     }, message);
 };
 
+
+var isSuccess = function(code) {
+    return code >= 200 && code <= 299;
+};
+
 var dash = function() {
         nanoajax.ajax({
             url: "/dashy",
             type: "POST",
-            body: JSON.stringify({
-                url: config.url,
-                interests: config.interests
-            })
+            body: requestBody
         }, function(code, responseText, request) {
-            newTree = (code >= 200 && code <= 299) ? render(JSON.parse(responseText)) : renderError(responseText || "Error!");
+            if (isSuccess(code)) {
+                newTree = renderPipeline(JSON.parse(responseText));
+            } else {
+                newTree = renderError(responseText || "Error!");
+            }
             patches = diff(tree, newTree);
             rootNode = patch(rootNode, patches);
             tree = newTree;
@@ -66,4 +78,4 @@ var dash = function() {
 
 document.body.appendChild(rootNode);
 dash();
-setInterval(dash, config.interval);
+setInterval(dash, interval);
