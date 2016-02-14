a9d118c762a124b22e6fdfa8062be0faa2e823d3
Chirantan Mitra <chirantan.mitra@gmail.com>
2016-02-14 22:12:50 +0530

    Use domchanger instead of virtual-dom
    The resulting javascript file (post uglify) is more than 60% smaller

--- public/main.js
+++ public/main.js
@@ -1,12 +1,6 @@
-var virtualDom = require("virtual-dom");
+var domChanger = require("domChanger");
 var nanoajax = require("nanoajax");
-
 var config = window.config || {};
-
-var h = virtualDom.h;
-var diff = virtualDom.diff;
-var createElement = virtualDom.create;
-var patch = virtualDom.patch;
 
 var requestBody = JSON.stringify({
     url: config.url,
@@ -14,60 +8,105 @@ var requestBody = JSON.stringify({
 });
 var interval = config.interval;
 
-var buildPipelineTree = function(pipeline) {
-    var stageNodes = pipeline.stages.map(function(stage) {
-        return h("div", {
-            className: "stage " + stage.status.toLowerCase()
-        }, [stage.name]);
-    });
-
-    return h("div", {
-        className: "pipeline"
-    }, [
-        h("div", {
-            className: "stage-container"
-        }, stageNodes),
-        h("div", {
-            className: "pipeline-name"
-        }, [pipeline.name])
-    ]);
-};
-
-var renderPipeline = function(pipelines) {
-    return h("div", {
-        id: "dashboard"
-    }, pipelines.map(buildPipelineTree));
-};
-
-var renderError = function(message) {
-    return h("div", {
-        id: "dashboard",
-        className: "error"
-    }, message);
-};
-
 var isSuccess = function(code) {
     return code >= 200 && code <= 299;
 };
 
-var dashy = (function() {
-    var tree = h("div", {
-        id: "dashboard"
-    });
-    var newTree = tree;
-    var patches = function() {
-        return diff(tree, newTree);
+var DashyError = function() {
+    var errorProps = {
+        id: "dashboard",
+        class: "error"
     };
-    var rootNode = createElement(tree);
+    var render = function(message) {
+        return ["div", errorProps, message];
+    };
 
+    return {
+        render: render
+    };
+};
+
+var Stage = function() {
+    var render = function(stage) {
+        var stageProps = {
+            class: "stage " + stage.status.toLowerCase()
+        };
+        return ["div", stageProps, stage.name];
+    };
+
+    return {
+        render: render
+    };
+};
+
+var StageList = function() {
+    var stageContainerProps = {
+        class: "stage-container"
+    };
+    var render = function(stages) {
+        return ["div", stageContainerProps, stages.map(function(stage) {
+            return [Stage, stage];
+        })];
+    };
+
+    return {
+        render: render
+    };
+};
+
+var PipelineName = function() {
+    var pipelineNameProps = {
+        class: "pipeline-name"
+    };
+    var render = function(name) {
+        return ["div", pipelineNameProps, name];
+    };
+
+    return {
+        render: render
+    };
+};
+
+var Pipeline = function() {
+    var pipelineProps = {
+        class: "pipeline"
+    };
+    var render = function(pipeline) {
+        return ["div", pipelineProps, [
+            [StageList, pipeline.stages],
+            [PipelineName, pipeline.name]
+        ]];
+    };
+
+    return {
+        render: render
+    };
+};
+
+var PipelineList = function() {
+    var pipelineListProps = {
+        id: "dashboard"
+    };
+    var render = function(pipelines) {
+        return ["div", pipelineListProps, pipelines.map(function(pipeline) {
+            return [Pipeline, pipeline];
+        })];
+    };
+
+    return {
+        render: render
+    };
+};
+
+var Dashy = function(emit, refresh) {
+    var renderedItem = ["div", "Initializing..."];
     var responseHandler = function(code, responseText, request) {
         if (isSuccess(code)) {
-            newTree = renderPipeline(JSON.parse(responseText));
+            renderedItem = [PipelineList, JSON.parse(responseText)];
         } else {
-            newTree = renderError(responseText || "Error!");
+            renderedItem = [DashyError, (responseText || "Error!")];
         }
-        rootNode = patch(rootNode, patches());
-        tree = newTree;
+        refresh();
         console.log("tick!");
     };
     var ajaxOptions = {
@@ -75,17 +114,19 @@ var dashy = (function() {
         type: "POST",
         body: requestBody
     };
-
     var tick = function() {
         nanoajax.ajax(ajaxOptions, responseHandler);
     };
-
-
-    var init = function() {
-        document.body.appendChild(rootNode);
-        tick();
-        setInterval(tick, interval);
+    var render = function() {
+        return renderedItem;
     };
 
-    init();
-}());
+    tick();
+    setInterval(tick, interval);
+    return {
+        render: render
+    };
+};
+
+var instance = domChanger(Dashy, document.getElementById("app"));
+instance.update();
