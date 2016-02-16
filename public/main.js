dc2b0fc00f735e26725956b92700f2d63bd08ad7
Chirantan Mitra <chirantan.mitra@gmail.com>
2016-02-16 23:45:29 +0530

    Pipeline configurable base size from which all other sizes are calculated
    fixes #3

--- public/main.js
+++ public/main.js
@@ -8,9 +8,26 @@ var requestBody = JSON.stringify({
 });
 var interval = config.interval || 30000;
 var groupSize = config.groupSize || 1;
+var baseSize = config.baseSize || 6;
 
 var isSuccess = function(code) {
     return code >= 200 && code <= 299;
+};
+
+var initCSS = function() {
+    var a1 = baseSize;
+    var a2 = baseSize * 0.986111;
+    var a3 = baseSize * 0.833334;
+    var a4 = baseSize * 0.125;
+
+    var cssContent = ".pipeline-name {height: " + a1 + "vmax; margin-top: " + (-a2) + "vmax; font-size: " + a3 + "vmax;}" +
+        " .stage-container {height: " + a1 + "vmax;}" +
+        " .stage {font-size: " + a4 + "vmax;}";
+
+    var style = document.createElement("style");
+    style.type = "text/css";
+    style.innerHTML = cssContent;
+    document.getElementsByTagName("head")[0].appendChild(style);
 };
 
 var asError = function(message, code) {
@@ -138,4 +155,5 @@ var Dashy = function(emit, refresh) {
     };
 };
 
+initCSS();
 domChanger(Dashy, document.getElementById("app")).update();
